7e249557fe001214f542e24c1497b7ddd9a68874
Colt Steele <coltsteele1@gmail.com>
2014-11-20 15:59:42 -0800

    Use Loop to create listeners

--- calculator.js
+++ calculator.js
@@ -3,83 +3,28 @@ window.onload = function() {
   var display = document.getElementById("result_display_value");
   var operator = document.getElementById("operator_display");
   var calc = document.querySelector("#container")
-  // Number buttons
-  var numOne = document.querySelector("#number_1"),
-      numTwo = document.querySelector("#number_2"),
-      numThree = document.querySelector("#number_3"),
-      numFour = document.querySelector("#number_4"),
-      numFive = document.querySelector("#number_5"),
-      numSix = document.querySelector("#number_6"),
-      numSeven = document.querySelector("#number_7"),
-      numEight = document.querySelector("#number_8"),
-      numNine = document.querySelector("#number_9"),
-      numZero = document.querySelector("#number_0");
   // Operations
-  var addOp = document.querySelector("#op_plus"),
-      subOp = document.querySelector("#op_sub"),
-      multOp = document.querySelector("#op_mult"),
-      divOp = document.querySelector("#op_div"),
-      entOp = document.querySelector("#op_ent"),
+  var entOp = document.querySelector("#op_ent"),
       clrOp = document.querySelector("#op_clr");
   // state
   var hiddenOperand;
-  
-  // event handlers
-  numOne.onclick = function() {
-    display.innerHTML += '1';
-  };
-  numTwo.onclick = function() {
-    display.innerHTML += '2';
-  };
-  numThree.onclick = function() {
-    display.innerHTML += '3';
-  };
-  numFour.onclick = function() {
-    display.innerHTML += '4';
-  };
-  numFive.onclick = function() {
-    display.innerHTML += '5';
-  };
-  numSix.onclick = function() {
-    display.innerHTML += '6';
-  };
-  numSeven.onclick = function() {
-    display.innerHTML += '7';
-  };
-  numEight.onclick = function() {
-    display.innerHTML += '8';
-  };
-  numNine.onclick = function() {
-    display.innerHTML += '9';
-  };
-  numZero.onclick = function() {
-    display.innerHTML += '0';
-  };
-  addOp.onclick = function() {
-    hiddenOperand = display.innerHTML;
-    console.log(hiddenOperand);
-    display.innerHTML = '';
-    operator.innerHTML = '+';
-  };
-  subOp.onclick = function() {
-    hiddenOperand = display.innerHTML;
-    console.log(hiddenOperand);
-    display.innerHTML = '';
-    operator.innerHTML = '-';
-  };
-  multOp.onclick = function() {
-    hiddenOperand = display.innerHTML;
-    console.log(hiddenOperand);
-    display.innerHTML = '';
-    operator.innerHTML = '*';
-  };
-  divOp.onclick = function() {
-    hiddenOperand = display.innerHTML;
-    console.log(hiddenOperand);
-    display.innerHTML = '';
-    operator.innerHTML = '/';
-  };
-  
+
+  //Number Button Setup
+  var numbers = document.querySelectorAll(".num") 
+  for(var i = 0; i<numbers.length;i++){
+    numbers[i].onclick = function(){
+      display.innerHTML += this.innerHTML;
+    }
+  }
+  //Operation Button Setup
+  var ops = document.querySelectorAll('div[id^="op"]');
+  for(var i = 0; i<ops.length;i++){
+    ops[i].onclick = function(){
+      hiddenOperand = display.innerHTML;
+      display.innerHTML = '';
+      operator.innerHTML = this.innerHTML
+    }
+  }
   entOp.onclick = function() {
     var result = parseInt(hiddenOperand);
     if (operator.innerHTML === '+') {
@@ -96,6 +41,7 @@ window.onload = function() {
     display.innerHTML = result;
     operator.innerHTML = '';
   };
+
   clrOp.onclick = function() {
     hiddenOperand = undefined;
     display.innerHTML = '';
