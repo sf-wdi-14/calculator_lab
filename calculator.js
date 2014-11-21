eeedef79bbc33d1482717daa89526c9a1c4955aa
Colt Steele <coltsteele1@gmail.com>
2014-11-20 21:20:35 -0800

    Add better solution branch

--- calculator.js
+++ calculator.js
@@ -1,50 +1,35 @@
 window.onload = function() {
-  // displays
-  var display = document.getElementById("result_display_value");
-  var operator = document.getElementById("operator_display");
-  var calc = document.querySelector("#container")
-  // Operations
-  var entOp = document.querySelector("#op_ent"),
-      clrOp = document.querySelector("#op_clr");
-  // state
   var hiddenOperand;
+  var display  = document.querySelector("#result_display_value");
+      operator = document.querySelector("#operator_display");
+      entOp    = document.querySelector("#op_ent"),
+      clrOp    = document.querySelector("#op_clr");
 
   //Number Button Setup
   var numbers = document.querySelectorAll(".num") 
-  for(var i = 0; i<numbers.length;i++){
-    numbers[i].onclick = function(){
+  for(var i = 0; i < numbers.length; i++) {
+    numbers[i].onclick = function() {
       display.innerHTML += this.innerHTML;
-    }
+    };
   }
   //Operation Button Setup
   var ops = document.querySelectorAll('div[id^="op"]');
-  for(var i = 0; i<ops.length;i++){
-    ops[i].onclick = function(){
-      hiddenOperand = display.innerHTML;
-      display.innerHTML = '';
-      operator.innerHTML = this.innerHTML
-    }
+  for(var i = 0; i<ops.length;i++) {
+    ops[i].onclick = function() {
+      hiddenOperand      = display.innerHTML;
+      display.innerHTML  = '';
+      operator.innerHTML = this.innerHTML;
+    };
   }
+
   entOp.onclick = function() {
-    var result = parseInt(hiddenOperand);
-    if (operator.innerHTML === '+') {
-      result += parseInt(display.innerHTML);
-    } else if (operator.innerHTML === '-') {
-      result -= parseInt(display.innerHTML);
-    } else if (operator.innerHTML === '*') {
-      result *= parseInt(display.innerHTML);
-    } else if (operator.innerHTML === '/') {
-      result /= parseInt(display.innerHTML);
-    } else {
-      result = display.innerHTML;
-    }
-    display.innerHTML = result;
+    display.innerHTML  = eval(parseInt(hiddenOperand) + operator.innerHTML + display.innerHTML);
     operator.innerHTML = '';
   };
 
   clrOp.onclick = function() {
-    hiddenOperand = undefined;
-    display.innerHTML = '';
+    hiddenOperand      = undefined;
+    display.innerHTML  = '';
     operator.innerHTML = '';
   };
 };
